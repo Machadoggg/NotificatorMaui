ee740cffbd667f81afae34c0028bd297eed3b32f
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-07 22:47:43 -0500

    Add Customer model

---
+++ Notificator/Customer.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notificator
+{
+    public class Customer
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del contacto es obligatorio.")]
+        [Column("customer_name")]
+        public string CustomerName { get; set; } = default!;
+
+        [Required(ErrorMessage = "El número del teléfono es obligatorio.")]
+        [Column("mobile")]
+        public string Mobile { get; set; } = default!;
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "El formato del correo electrónico no es válido.")]
+        [Column("email")]
+        public string Email { get; set; } = default!;
+    }
+}
